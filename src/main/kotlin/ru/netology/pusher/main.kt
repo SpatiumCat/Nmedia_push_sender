8a70753d32e24f5da1b4f5ba32c10973a4c4dc2a
Vladimir Baryshnikov <baryshnikovvladimirv@gmail.com>
2023-04-07 22:06:46 +0200

    Initial commit

--- src/main/kotlin/ru/netology/pusher/main.kt
+++ src/main/kotlin/ru/netology/pusher/main.kt
@@ -18,7 +18,7 @@ fun main() {
     val token = "eLg5Qg0sStiV0i1XDoWu3a:APA91bHPoVZQ2kUNq9HkhY9Tyc0AxxNWO4xk_Jsgx1GWnj5w_B1OSOEBnDjdB_Bblf6IVr9d4qt8nWeU7HxelCC9icZ_ISg2DKjojuGDGxHViCsAskkLmMuTlEOapo76a_2G_XT4YoIe"
 
     val message = Message.builder()
-        .putData("action", "POST")
+        .putData("action", "LIKE")
         .putData("content", """{
           "userId": 1,
           "userName": "Vasiliy",
@@ -28,5 +28,14 @@ fun main() {
         .setToken(token)
         .build()
 
-    FirebaseMessaging.getInstance().send(message)
+    val postMessage = Message.builder()
+        .putData("action", "POST")
+        .putData("content", """{
+            "postId": 1,
+            "author": "Vasiliy",
+            "content": "Привет. Это новая Нетология. Когда-то Нетология начиналась с интенсивов по онлайн маркетнигу. Затем появились курсы по дизайну и разработке, аналитике и управлению. Мы растем сами и помогаем расти студентам от новичков до уверенных профессионалов. Но самое важное остается с нами, мы верим, что в каждом уже есть сила,которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия - помочь встать на путь роста и начать цепочку перемен. https://netology.ru/",
+            "published": "Now"
+            }""".trimIndent()).setToken(token).build()
+
+    FirebaseMessaging.getInstance().send(postMessage)
 }
